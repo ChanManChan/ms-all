dc952282228eb67984d2800587a5ed971cc69656
ChanManChan <ngopal253@gmail.com>
2020-09-20 20:05:58 +0530

    base URL change

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,8 @@ export default ({ req }) => {
   if(typeof window === 'undefined') {
     // On the server
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.countervalve.com/',
       headers: req.headers
     })
   } else {
